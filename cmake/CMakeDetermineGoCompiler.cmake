845e83912cfb2d7cc59c3c196c604117e1d42dfa
Seidko <seidko.coding@gmail.com>
2024-07-31 14:54:47 +0800

    CMake adapt windows

--- cmake/CMakeDetermineGoCompiler.cmake
+++ cmake/CMakeDetermineGoCompiler.cmake
@@ -28,7 +28,7 @@ if(NOT CMAKE_Go_COMPILER)
       NAMES go
       PATHS ${Go_BIN_PATH}
     )
-    EXECUTE_PROCESS(${CMAKE_Go_COMPILER} ARGS version OUTPUT_VARIABLE GOLANG_VERSION)
+    EXECUTE_PROCESS(COMMAND ${CMAKE_Go_COMPILER} version OUTPUT_VARIABLE GOLANG_VERSION)
     STRING(REGEX MATCH "go[0-9]+.[0-9]+.[0-9]+[ /A-Za-z0-9]*" VERSION "${GOLANG_VERSION}")
     message("-- The Golang compiler identification is ${VERSION}")
     message("-- Check for working Golang compiler: ${CMAKE_Go_COMPILER}")
--- cmake/golang.cmake
+++ cmake/golang.cmake
@@ -6,42 +6,70 @@ function(ExternalGoProject_Add TARG)
 endfunction(ExternalGoProject_Add)
 
 function(add_go_executable NAME)
+  get_filename_component(CMAKE_C_COMPILER_NAME ${CMAKE_C_COMPILER} NAME)
+  get_filename_component(CMAKE_CXX_COMPILER_NAME ${CMAKE_CXX_COMPILER} NAME)
   file(GLOB GO_SOURCE RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
-  add_custom_command(OUTPUT ${OUTPUT_DIR}/.timestamp 
-    COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
-    -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
-    ${CMAKE_GO_FLAGS} ${GO_SOURCE}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
-
+  if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    ADD_CUSTOM_COMMAND(OUTPUT ${OUTPUT_DIR}/.timestamp
+      COMMAND set "GOPATH=\"${GOPATH}\""
+      COMMAND set "CGO_ENABLED=${CGO_ENABLED}"
+      COMMAND ${CMAKE_Go_COMPILER} env -w CC=${CMAKE_C_COMPILER_NAME}
+      COMMAND ${CMAKE_Go_COMPILER} env -w CXX=${CMAKE_CXX_COMPILER_NAME}
+      COMMAND ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
+      -o "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}"
+      ${CMAKE_GO_FLAGS} ${GO_SOURCE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+  else ()
+    add_custom_command(OUTPUT ${OUTPUT_DIR}/.timestamp
+      COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
+      -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
+      ${CMAKE_GO_FLAGS} ${GO_SOURCE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+  endif ()
   add_custom_target(${NAME} ALL DEPENDS ${OUTPUT_DIR}/.timestamp ${ARGN})
   install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
 endfunction(add_go_executable)
 
-
 function(ADD_GO_LIBRARY NAME BUILD_TYPE)
-  if(BUILD_TYPE STREQUAL "STATIC")
+  if (BUILD_TYPE STREQUAL "STATIC")
     set(BUILD_MODE -buildmode=c-archive)
     set(LIB_NAME "lib${NAME}.a")
-  else()
+  else ()
     set(BUILD_MODE -buildmode=c-shared)
-    if(APPLE)
+    if (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
       set(LIB_NAME "lib${NAME}.dylib")
-    else()
+    elseif (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+      set(LIB_NAME "lib${NAME}.dll")
+    else ()
       set(LIB_NAME "lib${NAME}.so")
-    endif()
-  endif()
+    endif ()
+  endif ()
 
+  get_filename_component(CMAKE_C_COMPILER_NAME ${CMAKE_C_COMPILER} NAME)
+  get_filename_component(CMAKE_CXX_COMPILER_NAME ${CMAKE_CXX_COMPILER} NAME)
   file(GLOB GO_SOURCE RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
-  ADD_CUSTOM_COMMAND(OUTPUT ${OUTPUT_DIR}/.timestamp
-    COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
-    -o "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}"
-    ${CMAKE_GO_FLAGS} ${GO_SOURCE}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+  if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    ADD_CUSTOM_COMMAND(OUTPUT ${OUTPUT_DIR}/.timestamp
+      COMMAND set "GOPATH=\"${GOPATH}\""
+      COMMAND set "CGO_ENABLED=${CGO_ENABLED}"
+      COMMAND ${CMAKE_Go_COMPILER} env -w CC=${CMAKE_C_COMPILER_NAME}
+      COMMAND ${CMAKE_Go_COMPILER} env -w CXX=${CMAKE_CXX_COMPILER_NAME}
+      COMMAND ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
+      -o "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}"
+      ${CMAKE_GO_FLAGS} ${GO_SOURCE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+  else ()
+    ADD_CUSTOM_COMMAND(OUTPUT ${OUTPUT_DIR}/.timestamp
+      COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
+      -o "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}"
+      ${CMAKE_GO_FLAGS} ${GO_SOURCE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+  endif ()
 
   ADD_CUSTOM_TARGET(${NAME} ALL DEPENDS ${OUTPUT_DIR}/.timestamp ${ARGN})
   set_property(TARGET ${NAME} PROPERTY "type" ${BUILD_TYPE})
 
-  if(NOT BUILD_TYPE STREQUAL "STATIC")
+  if (NOT BUILD_TYPE STREQUAL "STATIC")
     install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME} DESTINATION bin)
-  endif()
+  endif ()
 endfunction(ADD_GO_LIBRARY)
